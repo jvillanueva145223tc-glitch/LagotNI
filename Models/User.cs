037d597e515b13b7cbb380acac1bf970d376dd5c
jvillanueva145223tc-glitch <j.villanueva.145223.tc@umindanao.edu.ph>
2025-12-07 09:43:34 +0800

    Adding Classes

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagotNI.Models
+{
+    public abstract class User
+    {
+        public int UserId { get; set; };
+        public string Username { get; set; };
+        public string PasswordHash { get; set; };
+        public string FullName { get; set; };
+    }
+}
